b558db8c3fb4ae27ed03ed7ea24f93eb34dc5e75
cixiangtao <w_cixiangtao.xiwang.com>
2022-12-06 23:47:46 +0800

    chore: :hammer: chore

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import { defineConfig } from "vite";
 import typescript from "rollup-plugin-typescript2";
 
 export default defineConfig({
-  plugins: [
-    typescript({
-      rollupCommonJSResolveHack: false,
-      clean: false,
-    }),
-  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
