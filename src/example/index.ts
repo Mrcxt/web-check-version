6655fee1052b75dc16383a46c9dbd023c0f935b5
cixiangtao <w_cixiangtao.xiwang.com>
2022-12-07 17:50:17 +0800

    fix: :bug: fix

---
+++ src/example/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @LastEditTime: 2022-12-07 17:50:03
+ * @Description:
+ * @Date: 2022-12-07 17:47:47
+ * @Author: @虾哔哔
+ */
+import { webVersionCheck } from "web-check-version";
+
+webVersionCheck({});
